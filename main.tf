2b87c4eca1c608a92740b8a69ae98a60d1e1dda3
jim.kariotis@gmail.com <jim.kariotis@gmail.com>
2019-09-20 01:20:12 +0000

    4

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-
-resource "azurerm_resource_group" "resource_gp" { # "resource_gp" is the name of this resource, you can use it to call it for other resources
+resource "azurerm_resource_group" "resource_gp" {
+  # "resource_gp" is the name of this resource, you can use it to call it for other resources
   name     = "Azure-Demo"
   location = "eastus"
-
   tags = {
     Owner = "Jim K"
   }
 }
-
 
 variable "prefix" {
   default = "MAP"
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network" #this will create a vnet called sl-network as it takes the "prefix" variable called above
-  address_space       = ["10.0.0.0/16"]
+  name = "${var.prefix}-network" #this will create a vnet called sl-network as it takes the "prefix" variable called above
+  address_space = [
+    "10.0.0.0/16",
+  ]
   location            = "${azurerm_resource_group.resource_gp.location}" #This is calling the "resource_gp" above and using it's location and name
   resource_group_name = "${azurerm_resource_group.resource_gp.name}"
 }
@@ -45,7 +45,6 @@ resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic" #This will be called sl-nic as per the varibale declared above
   location            = "${azurerm_resource_group.resource_gp.location}"
   resource_group_name = "${azurerm_resource_group.resource_gp.name}"
-
   ip_configuration {
     name                          = "testconfiguration1"
     subnet_id                     = "${azurerm_subnet.backend.id}"
@@ -54,18 +53,17 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm" #VM name will be MAP-vm
-  location              = "${azurerm_resource_group.resource_gp.location}"
-  resource_group_name   = "${azurerm_resource_group.resource_gp.name}"
-  network_interface_ids = ["${azurerm_network_interface.main.id}"]
-  vm_size               = "Standard_DS1_v2"
-
+  name                = "${var.prefix}-vm" #VM name will be MAP-vm
+  location            = "${azurerm_resource_group.resource_gp.location}"
+  resource_group_name = "${azurerm_resource_group.resource_gp.name}"
+  network_interface_ids = [
+    "${azurerm_network_interface.main.id}",
+  ]
+  vm_size = "Standard_DS1_v2"
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
-
   # Uncomment this line to delete the data disks automatically when deleting the VM
   delete_data_disks_on_termination = true
-
   storage_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
@@ -83,13 +81,10 @@ resource "azurerm_virtual_machine" "main" {
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
-  os_profile_windows_config {
-  }
-
+  os_profile_windows_config {}
   tags = {
     environment = "staging"
   }
-
   /*
   storage_image_reference {
     publisher = "Canonical"
@@ -114,4 +109,18 @@ resource "azurerm_virtual_machine" "main" {
   tags = {
     environment = "staging"
   } */
+}
+
+resource "azurerm_virtual_network" "hubVNET" {
+  address_space = [
+    "10.0.0.0/16",
+  ]
+  location            = "eastus"
+  name                = "hubvnet"
+  resource_group_name = "hub-rg"
+}
+
+resource "azurerm_resource_group" "hubrg" {
+  location = "eastus"
+  name     = "hub-rg"
 }
